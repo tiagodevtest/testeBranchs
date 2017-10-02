16389e6914f8cede76ffbdaf86cf852826ae5573
Tiago Pereira <Tiago Pereira>
2017-10-02 20:33:38 -0300

    coloquei tres rrr no mostrar e acrescentei na primeira linha

--- scripts_sql.sql
+++ scripts_sql.sql
@@ -1,3 +1,3 @@
+Essa linha eu estou acrescentando no 10.10 manat (tem três r no mostrar )
 
-
-esssa é uma sub feature do manat-01, para mostrar como ficaria o git flow.
+esssa é uma sub feature do manat-01, para mostrarrr como ficaria o git flow.
