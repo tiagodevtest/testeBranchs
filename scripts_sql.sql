ebf297420f225bdae330e344356b59345f4b809b
Tiago Pereira <Tiago Pereira>
2017-10-07 20:08:26 -0300

    Conflitos causados pelo merge do stash salvo no manat-01

--- scripts_sql.sql
+++ scripts_sql.sql
@@ -1,3 +1,18 @@
-
-
 esssa é uma sub feature do manat-01, para mostrar como ficaria o git flow.
+
+Is it cheating if you erase the memory? This week's exclusive short film "Never Happened" by Mark Slutsky explores how technology of the near-future affects the decisions we make and our most precious relationships.
+1
+2
+3
+4
+5
+6
+7
+Essa linha eu estou acrescentando no 01.01 manat.
+
+Esse é o branch do developer
+
+
+
+
+Essa é uma sub feature do manat-01, para mostrar como ficaria o git flow.
